aa5401db1093b6e2e945c1db9187b8fd3c6c122c
densetsu <tab.23.yude@gmail.com>
2022-05-25 23:52:49 +0900

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+      <button>ボタン</button>
+      <Button>ボタン</Button>
+    </>
+  );
+}
+
+export default App;
